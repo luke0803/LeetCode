d5cf9a62c6392de54488480edd7e7a1ceac00f3d
Luke <luke_luke82@hotmail.com>
2018-06-29 11:21:08 +0800

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+   
+    stack<char> stk;
+    stk.push('#');
+    char top;
+        for(char c : s)
+        //for(int i=0; i < s.size(); i++)
+        {
+            top = stk.top();
+                 if(top == '(' && c == ')') stk.pop(); 
+            else if(top == '[' && c == ']') stk.pop(); 
+            else if(top == '{' && c == '}') stk.pop();
+            else stk.push(c);
+        }
+        top = stk.top();
+        if(top == '#') return true;
+        else           return false;
+        
+    }
+};
