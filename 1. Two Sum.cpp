08657d8c0dfa1d2f73666ca3daf0cd388a12ac53
Luke <luke_luke82@hotmail.com>
2018-06-29 09:30:22 +0800

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         //Key is the number and value is its index in the vector.
+        //Key is the number and value is its index in the vector.
 	unordered_map<int, int> hash;
 	vector<int> result;
 	for (int i = 0; i < nums.size(); i++) {
 		int numberToFind = target - nums[i];
 
-            //if numberToFind is found in map, return them
+                //if numberToFind is found in map, return them
 		if (hash.find(numberToFind) != hash.end()) {
                     //+1 because indices are NOT zero based
 			result.push_back(hash[numberToFind]);
@@ -15,7 +15,7 @@ public:
 			return result;
 		}
 
-            //number was not found. Put it in the map.
+                //number was not found. Put it in the map.
 		hash[nums[i]] = i;
 	}
 	return result;
